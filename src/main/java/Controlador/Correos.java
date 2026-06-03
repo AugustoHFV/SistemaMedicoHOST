90913d3c3a20a79ffcc3c453175ae40ce1e05203
AugustoHFV <124326528+AugustoHFV@users.noreply.github.com>
2026-06-03 10:27:09 -0600

    Update Correos.java

--- src/main/java/Controlador/Correos.java
+++ src/main/java/Controlador/Correos.java
@@ -37,46 +37,11 @@ public class Correos {
         emailTo = email;
         subject = T;
         content = M;
-        
-         // Simple mail transfer protocol
-        mProperties.put("mail.smtp.host", "smtp.gmail.com");
-        mProperties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-        mProperties.setProperty("mail.smtp.starttls.enable", "true");
-        mProperties.setProperty("mail.smtp.port", "587");
-        mProperties.setProperty("mail.smtp.user",emailFrom);
-        mProperties.setProperty("mail.smtp.ssl.protocols", "TLSv1.2");
-        mProperties.setProperty("mail.smtp.auth", "true");
-        
-        mSession = Session.getDefaultInstance(mProperties);
-        
-        
-        try {
-            mCorreo = new MimeMessage(mSession);
-            mCorreo.setFrom(new InternetAddress(emailFrom));
-            mCorreo.setRecipient(Message.RecipientType.TO, new InternetAddress(emailTo));
-            mCorreo.setSubject(subject);
-            mCorreo.setText(content, "ISO-8859-1", "html");
-                     
-            
-        } catch (AddressException ex) {
-        } catch (MessagingException ex) {
-        }
     }
 
     private void sendEmail() {
-        try {
-            Transport mTransport = mSession.getTransport("smtp");
-            mTransport.connect(emailFrom, passwordFrom);
-            mTransport.sendMessage(mCorreo, mCorreo.getRecipients(Message.RecipientType.TO));
-            mTransport.close();
-            
-        } catch (NoSuchProviderException ex) {
-        } catch (MessagingException ex) {
-        }
     }
     
     public void Correo(String email,String T,String M) {
-    createEmail(email,T,M);
-    sendEmail();
     }
 }
